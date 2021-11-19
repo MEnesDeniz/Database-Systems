e0182b969357c1125d8053868b3ea72d9b2c6dd4
Muhammed Enes Deniz <menesdeniz99@gmail.com>
2021-11-19 23:33:04 +0300

    Post Method for airlines

--- views/airlines.py
+++ views/airlines.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, request, render_template
-
+import psycopg2 as db
+import os
 
 airlines = Blueprint('airlines',import_name=__name__,template_folder="templates")
 
-@airlines.route("/airlines")
+@airlines.route("/airlines",  methods=["GET", "POST"])
 def airlines_page():
-    return render_template("airlines_page.html")
+    connection = db.connect(os.getenv("DATABASE_URL"))
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM airlines")
+    list_airlines = cur.fetchall()
+    cur.close()
+    return render_template("airlines_page.html", list_airlines = list_airlines)
